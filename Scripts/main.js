ecc4d65a9241b09128575c919a12d23989ae7070
Emanuel <eg.dev.vs@gmail.com>
2023-06-22 08:41:36 -0500

    Initial commit

---
+++ Scripts/main.js
@@ -0,0 +1,92 @@
+// Undefined Example
+var camelCase1;
+
+// Boolean Example
+var camelCase2 = True;
+
+// Variable Example
+var camelCase3 = 0;
+
+// String Example
+var camelCase4 = "This is a string";
+
+// Constant Example
+const camelCase5 = 154;
+
+// Null Example
+var camelCase6 = null;
+
+// Single-Dimensional Array Example
+let arrayExample1 = ["first", "second", "third"];
+
+// Multi-Dimensional Array Example
+let arrayExample2 = [["first", "second"], ["third", "fourth"]];
+
+// Function Example
+function functionExample(parameter1, parameter2, parameter3) {
+    var argument1 = parameter1;
+    var argument2 = parameter2;
+    var argument3 = parameter3;
+};
+
+// Function Example
+function shortenedFunctionExample(parameter1) {if (parameter1 <= 0) return 'negative'; else return 'positive';}
+
+// Class Example
+class ClassExample {
+    constructor(parameter1, parameter2) {
+        this.parameter1 = parameter1;
+        this.parameter2 = parameter2;
+    }
+
+    distance() {
+        return Math.sqrt(
+            this.parameter1 * this.parameter1 +
+            this parameter2 * this.parameter2
+        );
+    }
+};
+
+// Variable Scope Example
+var varExample = true;
+
+function exampleFunction() {
+    let varExample = false;
+    console.log(varExample); // Returns false
+}
+
+console.log(globalExample()); // Returns true
+
+function openNewWindow() {
+    checkWin = window.open("Main.html", "", "width=300, height=300, status=no");
+}
+
+/* Functions and Methods */
+// typeOf() - Defines variable type.
+// Number.isInteger() - (Boolean) Is variable an integer?
+// "use strict" - Requires all variables to be defined.
+// parseFloat() - Converts objects to floating variable.
+// parseInt() - Converts objects to integer variable.
+// .toFixed() - Defines the exact decimal place of a floating variable.
+// concat() - Concatinates two strings.
+// eval() - Evaluates if string is an expression. (Do not use!)
+// toPrecision() - Defines the exact number of digits of a integer variable.
+// Date() - Defines date.
+// .toLocaleString - Returns a string with a language-sensitive representation of this date.
+// .sort - Sorts array alphabetically.
+// indexOf() - Returns index of item.
+// .push - Adds item to the end of an array.
+// .pop - Removes item from an array. If undefined, removes last item added to array.
+// .shift - Removes the first item from an array.
+// .unshift - Adds an item to beginning of an array.
+// constructor() - Initializes new instances of class.
+// .floor - Rounds number down.
+// .ceil - Rounds number up.
+// .min - Returns lowest number 
+// .max - Returns highest number 
+// .pow - Raises number to the specified power.
+// .sqrt - Squareroot's a number.
+
+/* Definitions */
+// Declaration - The creation of a variable.
+// Initialization - The storing of information in a variable.
