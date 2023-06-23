c977a505a9f7124eb0adb35d5c672617af5a0cd4
Emanuel <eg.dev.vs@gmail.com>
2023-06-23 11:32:32 -0500

    Transfered function fillTestResults from index.html to main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,3 +1,12 @@
 function openNewWindow() {
     checkWin = window.open("Main.html", "", "width=300, height=300, status=no");
 }
+
+function fillTestResults() {
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var emailAddress = document.getElementById('emailAddress').value;
+
+    testResults.innerHTML = firstName + " " + lastName + " " + emailAddress;
+}
+
